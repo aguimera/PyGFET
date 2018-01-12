1296bd408fa7dbf3a798377fa30eae59c2d569bb
aguimera <anton.guimera@csic.es>
2018-01-12 14:41:19 +0100

    Split DBanalyze module

--- PyGFET/DBAnalyze.py
+++ PyGFET/DBAnalyze.py
@@ -15,8 +15,7 @@ from itertools import cycle
 import statsmodels.api as sm
 import xlsxwriter as xlsw
 
-from PyGFET.DataClass import DataCharAC
-import PyGFET.DBCore as PyFETdb
+from PyGFET.DBSearch import GetFromDB
 
 
 def PlotMeanStd(Data, Xvar, Yvar, Vds=None, Ax=None, Ud0Norm=True, Color='r',
@@ -179,104 +178,6 @@ def GetParam(Data, Param, Vgs=None, Vds=None, Ud0Norm=False, **kwargs):
     return Vals
 
 
-def CheckConditionsCharTable(Conditions, Table):
-    for k in Conditions.keys():
-        if k.startswith('CharTable'):
-            nk = k.replace('CharTable', Table)
-            Conditions.update({nk: Conditions[k]})
-            del(Conditions[k])
-    return Conditions
-
-
-def FindCommonParametersValues(Parameter, Conditions, Table='ACcharacts'):
-    Conditions = CheckConditionsCharTable(Conditions, Table)
-
-    if Parameter.startswith('CharTable'):
-        Parameter = Parameter.replace('CharTable', Table)
-
-    MyDb = PyFETdb.PyFETdb(host='opter6.cnm.es',
-                           user='pyfet',
-                           passwd='p1-f3t17',
-                           db='pyFET')
-#    MyDb = PyFETdb.PyFETdb()
-
-    Output = (Parameter,)
-    Res = MyDb.GetCharactInfo(Table=Table,
-                              Conditions=Conditions,
-                              Output=Output)
-
-    del (MyDb)
-    #  Generate a list of tupples with devices Names and comments
-    Values = []
-    for Re in Res:
-        Values.append(Re[Parameter])
-
-    return set(Values)
-
-
-def GetFromDB(Conditions, Table='ACcharacts', Last=True, GetGate=True,
-              OutilerFilter=None):
-    Conditions = CheckConditionsCharTable(Conditions, Table)
-
-    MyDb = PyFETdb.PyFETdb(host='opter6.cnm.es',
-                           user='pyfet',
-                           passwd='p1-f3t17',
-                           db='pyFET')
-
-#    MyDb = PyFETdb.PyFETdb()
-
-    DataD, Trts = MyDb.GetData2(Conditions=Conditions,
-                                Table=Table,
-                                Last=Last,
-                                GetGate=GetGate)
-
-    del(MyDb)
-
-    Data = {}
-    for Trtn, Cys in DataD.iteritems():
-        print Trtn
-        Chars = []
-        for Cyn, Dat in Cys.iteritems():
-            Char = DataCharAC(Dat)
-            Chars.append(Char)
-        Data[Trtn] = Chars
-
-    if OutilerFilter is None:
-        return Data, Trts
-
-#   Find Outliers
-    Vals = np.array([])
-    for Trtn, Datas in Data.iteritems():
-        for Dat in Datas:
-            func = Dat.__getattribute__('Get' + OutilerFilter['Param'])
-            Val = func(Vgs=OutilerFilter['Vgs'],
-                       Vds=OutilerFilter['Vds'],
-                       Ud0Norm=OutilerFilter['Ud0Norm'])
-            if Val is not None:
-                Vals = np.hstack((Vals, Val)) if Vals.size else Val
-
-    p25 = np.percentile(Vals, 25)
-    p75 = np.percentile(Vals, 75)
-    lower = p25 - 1.5 * (p75 - p25)
-    upper = p75 + 1.5 * (p75 - p25)
-
-    Data = {}
-    for Trtn, Cys in DataD.iteritems():
-        Chars = []
-        for Cyn, Dat in Cys.iteritems():
-            Char = DataCharAC(Dat)
-            func = Char.__getattribute__('Get' + OutilerFilter['Param'])
-            Val = func(Vgs=OutilerFilter['Vgs'],
-                       Vds=OutilerFilter['Vds'],
-                       Ud0Norm=OutilerFilter['Ud0Norm'])
-
-            if (Val <= lower or Val >= upper):
-                print 'Outlier Removed ->', Val, Trtn, Cyn
-            else:
-                Chars.append(Char)
-        Data[Trtn] = Chars
-
-    return Data, Trts
 
 
 def MultipleSearchParam(Plot=True, Boxplot=False, **kwargs):
