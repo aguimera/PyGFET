Fix ILoad calculation

PyGFET/StimCore.py
@@ -421,21 +421,21 @@ class DataProcess(ChannelsConfig):
     def CalcDcContData(self, Data):
         print 'DataProcess CalcDCContData'
         print np.mean(Data)
+        print self.Rds
 
-        if self.Rharware:
-            print self.Rds
-            Vload = Data * (self.Rds/(self.Rds+1e3))
-            Iload = Data * (1/(self.Rds+1e3))
+#        if self.Rharware:
+#            print self.Rds
+#            Vload = Data * (self.Rds/(self.Rds+1e3))
+#        else:
+#            print 'no RDS'
+#            Vload = Data + self.IVGainDC*self.Vin
 
-        else:
-            print 'no RDS'
-            Vload = Data + self.IVGainDC*self.Vin
-            Iload = (self.IVGainDC*self.Vin)/1e3
-
-        print np.mean(Iload), 'Iload'
+        Vload = Data * (self.Rds/(self.Rds+1e3))
+        print 'Vload ->', np.mean(Vload)
+        Iload = Data * (1/(self.Rds+1e3))
 
         if self.EventContDcDone:
-            self.EventContDcDone(Vload)
+            self.EventContDcDone(Iload)
 
 
 ###############################################################################